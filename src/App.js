b36c29ab5b3305fac50dd26481c9db5d5691e9b0
Felipe Llerena <llerenafelipe7@gmail.com>
2019-06-03 12:02:20 -0500

    Create Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Header from './components/Header'
 
 class App extends Component {
   render (){
     return (
       <div className="App">
-        <h1>Hi!</h1>
+        <Fragment>
+          <Header/>
+        </Fragment>
       </div>
     );
   }
